bc0c1f70855ee38e0020da5ee77202cb2fe80618
rreichmann34 <rreichmann@wisc.edu>
2026-03-18 16:03:57 -0500

    Fixes

--- TrenZone/vite.config.ts
+++ TrenZone/vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TrenZone',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: "docs"
-  }
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom', 'react-router-dom'],
+  },
 })
